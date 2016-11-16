03c9eb82745e966e5be2969c18727d51176d9dc5
Sean O'Keeffe <seanokeeffe797@gmail.com>
2016-11-16 13:24:11 +0000

    Fixes #17359 - Tracer yum plugin upload should look at all pkgs

--- src/yum-plugins/tracer_upload.py
+++ src/yum-plugins/tracer_upload.py
@@ -21,10 +21,17 @@ def query_apps(conduit):
     """Returns all apps that need restarting """
     query = Query()
     if conduit:
-        pkgs = conduit.getTsInfo().getMembers()
+        # When running via yum we need to pass tracer a list of packages and 
+        # their last modified time so it has no need to access the rpmdb (which
+        # would fail as yum/dnf has a lock on it) 
         packages = []
+        pkgs = conduit.getTsInfo().getMembers() # Packages in the current transation
         for pkg in pkgs:
             pkg.modified = time.time()
+            packages.append(pkg)
+        rpmdb = conduit.getRpmDB() # All other packages
+        for pkg in rpmdb:
+            pkg.modified = pkg.installtime
             packages.append(pkg)
         return query.from_packages(packages).now().affected_applications().get()
     else:
