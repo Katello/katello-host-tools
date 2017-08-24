e17322c45a3e9b0f0146b56423870b50b022722c
Eric D. Helms <ericdhelms@gmail.com>
2017-08-24 12:32:54 -0400

    Bump version to 3.0.2

--- src/setup.py
+++ src/setup.py
@@ -29,7 +29,7 @@ requires = [
 
 setup(
     name='katello-agent',
-    version='3.0.1',
+    version='3.0.2',
     description='Katello Agent',
     author='Jeff Ortel',
     author_email='jortel@redhat.com',
