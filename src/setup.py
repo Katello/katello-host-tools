c2a3bccb5de4107bad0e34993a1a5b8b7cc235c2
Justin Sherrill <jsherril@redhat.com>
2018-04-16 15:21:01 -0400

    3.2.1 release

--- src/setup.py
+++ src/setup.py
@@ -29,7 +29,7 @@ requires = [
 
 setup(
     name='katello-agent',
-    version='3.2.0',
+    version='3.2.1',
     description='Katello Agent',
     author='Jeff Ortel',
     author_email='jortel@redhat.com',
