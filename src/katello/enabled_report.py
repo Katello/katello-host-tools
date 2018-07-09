f4eb180908562539a7698056c441a3c490f936a0
Chris Duryee <cduryee@redhat.com>
2018-07-09 18:11:05 -0400

    Fixes #24214 - fill in vars on repo URLs
    Previously, repo baseurls were sent as-is from redhat.repo to
    Satellite. This resulted in the client reporting it was subscribed to
    repos that don't exist, since the URLs would have the literal string
    `$releasever/$basearch` in it.
    
    This commit adds dnf and yum code to populate `$releasever` and
    `$basearch`. It will attempt to use DNF libraries first, and if that
    raises an `ImportError`, it will try yum.

--- src/katello/enabled_report.py
+++ src/katello/enabled_report.py
@@ -13,7 +13,7 @@ class EnabledReport(object):
         config = ConfigParser()
         config.read(self.repofile)
         enabled_sections = [section for section in config.sections() if config.getboolean(section, "enabled")]
-        enabled_repos = [{"repositoryid": section,"baseurl": [config.get(section, "baseurl")]} for section in enabled_sections]
+        enabled_repos = [{"repositoryid": section,"baseurl": [self._replace_vars(config.get(section, "baseurl"))]} for section in enabled_sections]
         return {"enabled_repos": {"repos": enabled_repos}}
 
     def __init__(self, repo_file = REPOSITORY_PATH):
@@ -26,3 +26,32 @@ class EnabledReport(object):
 
     def __str__(self):
         return str(self.content)
+
+    def _replace_vars(self, repo_url):
+        """
+        returns a string with "$basearch" and "$releasever" replaced.
+
+        :param repo_url: a repo URL that you want to replace $basearch and $releasever in.
+        :type path: str
+        """
+        mappings = self._obtain_mappings()
+        return repo_url.replace('$releasever', mappings['releasever']).replace('$basearch', mappings['basearch'])
+
+    def _obtain_mappings(self):
+        """
+        returns a hash with "basearch" and "releasever" set. This will try dnf first, and them yum if dnf is not installed.
+        """
+        try:
+            return self._obtain_mappings_dnf()
+        except ImportError:
+            return self._obtain_mappings_yum()
+
+    def _obtain_mappings_dnf(self):
+        import dnf
+        db = dnf.dnf.Base()
+        return {'releasever': db.conf.substitutions['releasever'], 'basearch': db.conf.substitutions['basearch']}
+
+    def _obtain_mappings_yum(self):
+        import yum
+        yb = yum.YumBase()
+        return {'releasever': yb.conf.yumvar['releasever'], 'basearch': yb.conf.yumvar['basearch']}
