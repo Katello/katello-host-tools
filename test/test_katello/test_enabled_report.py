1ffbd8afe9dfce6cf69f9b3bfed840a8e340256b
Chris Duryee <cduryee@redhat.com>
2018-07-09 18:11:05 -0400

    Fixes #24214 - fill in vars on repo URLs
    Previously, repo baseurls were sent as-is from redhat.repo to
    Satellite. This resulted in the client reporting it was subscribed to
    repos that don't exist, since the URLs would have the literal string
    `$releasever/$basearch` in it.
    
    This commit adds dnf and yum code to populate `$releasever` and
    `$basearch`. It will attempt to use DNF libraries first, and if that
    raises an `ImportError`, it will try yum.

--- test/test_katello/test_enabled_report.py
+++ test/test_katello/test_enabled_report.py
@@ -19,3 +19,23 @@ class TestEnabledReport(TestCase):
                                'repositoryid': 'enabled'}]}}
 
         self.assertEqual(expected, report.content)
+
+    @patch('katello.enabled_report.EnabledReport._obtain_mappings_dnf')
+    def test_var_interpolation_dnf(self, mock_obtain_mappings_dnf):
+        mock_obtain_mappings_dnf.return_value = {'releasever': "6.22", 'basearch': "80286"}
+        rh_repo = os.path.join(os.path.dirname(__file__), 'data/repos/redhat.repo.with_vars')
+        report = enabled_report.EnabledReport(rh_repo)
+        expected = {'enabled_repos': {'repos': [{'baseurl': ['https://enabled_repo.com/6.22/80286'], 'repositoryid': 'enabled_one'}]}}
+
+        self.assertEqual(expected, report.content)
+
+    @patch('katello.enabled_report.EnabledReport._obtain_mappings_yum')
+    @patch('katello.enabled_report.EnabledReport._obtain_mappings_dnf')
+    def test_var_interpolation_yum(self, mock_obtain_mappings_dnf, mock_obtain_mappings_yum):
+        mock_obtain_mappings_dnf.side_effect = ImportError("dnf not available on 80286")
+        mock_obtain_mappings_yum.return_value = {'releasever': "6.22", 'basearch': "80286"}
+        rh_repo = os.path.join(os.path.dirname(__file__), 'data/repos/redhat.repo.with_vars')
+        report = enabled_report.EnabledReport(rh_repo)
+        expected = {'enabled_repos': {'repos': [{'baseurl': ['https://enabled_repo.com/6.22/80286'], 'repositoryid': 'enabled_one'}]}}
+
+        self.assertEqual(expected, report.content)
