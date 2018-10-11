4542107d6a5091b4f8c153185a6a677ea690c112
Jonathon Turel <jturel@redhat.com>
2018-10-11 16:01:14 -0400

    Fixes #25089 - Remove DNF aspects

--- src/katello/enabled_report.py
+++ src/katello/enabled_report.py
@@ -1,22 +1,23 @@
-import os.path, sys
+import os.path
+from ConfigParser import ConfigParser
+
 from katello.constants import REPOSITORY_PATH
-if sys.version_info[0] == 3:
-    from configparser import ConfigParser
-else:
-    from ConfigParser import ConfigParser
+
+import yum
+
 
 class EnabledReport(object):
     def __generate(self):
         if not os.path.exists(self.repofile):
-            return {"enabled_repos": {"repos":[]}}
+            return {"enabled_repos": {"repos": []}}
 
         config = ConfigParser()
         config.read(self.repofile)
         enabled_sections = [section for section in config.sections() if config.getboolean(section, "enabled")]
-        enabled_repos = [{"repositoryid": section,"baseurl": [self._replace_vars(config.get(section, "baseurl"))]} for section in enabled_sections]
+        enabled_repos = [{"repositoryid": section, "baseurl": [self._replace_vars(config.get(section, "baseurl"))]} for section in enabled_sections]
         return {"enabled_repos": {"repos": enabled_repos}}
 
-    def __init__(self, repo_file = REPOSITORY_PATH):
+    def __init__(self, repo_file=REPOSITORY_PATH):
         """
         :param path: A .repo file path used to filter the report.
         :type path: str
@@ -34,24 +35,7 @@ class EnabledReport(object):
         :param repo_url: a repo URL that you want to replace $basearch and $releasever in.
         :type path: str
         """
-        mappings = self._obtain_mappings()
-        return repo_url.replace('$releasever', mappings['releasever']).replace('$basearch', mappings['basearch'])
-
-    def _obtain_mappings(self):
-        """
-        returns a hash with "basearch" and "releasever" set. This will try dnf first, and them yum if dnf is not installed.
-        """
-        try:
-            return self._obtain_mappings_dnf()
-        except ImportError:
-            return self._obtain_mappings_yum()
-
-    def _obtain_mappings_dnf(self):
-        import dnf
-        db = dnf.dnf.Base()
-        return {'releasever': db.conf.substitutions['releasever'], 'basearch': db.conf.substitutions['basearch']}
-
-    def _obtain_mappings_yum(self):
-        import yum
         yb = yum.YumBase()
-        return {'releasever': yb.conf.yumvar['releasever'], 'basearch': yb.conf.yumvar['basearch']}
+        mappings = {'releasever': yb.conf.yumvar['releasever'], 'basearch': yb.conf.yumvar['basearch']}
+
+        return repo_url.replace('$releasever', mappings['releasever']).replace('$basearch', mappings['basearch'])
--- test/test_katello/test_enabled_report.py
+++ test/test_katello/test_enabled_report.py
@@ -1,39 +1,34 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../src/'))
-from unittest import TestCase
-
-from katello import enabled_report
-
+import unittest2 as unittest
 from mock import patch
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../src/'))
 
-class TestEnabledReport(TestCase):
+try:
+    from katello import enabled_report
+except ImportError:
+    print("Yum wasn't present")
+
+
+class TestEnabledReport(unittest.TestCase):
+    @unittest.skipIf('yum' not in sys.modules, "Yum not present")
     def test_root_not_exists(self):
         report = enabled_report.EnabledReport("wrong path")
         self.assertEqual({'enabled_repos': {'repos': []}}, report.content)
 
+    @unittest.skipIf('yum' not in sys.modules, "Yum not present")
     def test_valid(self):
         rh_repo = os.path.join(os.path.dirname(__file__), 'data/repos/redhat.repo')
         report = enabled_report.EnabledReport(rh_repo)
         expected = {'enabled_repos': {'repos': [{'baseurl': ['https://enabled_repo.com'],
-                               'repositoryid': 'enabled'}]}}
+                                                 'repositoryid': 'enabled'}]}}
 
         self.assertEqual(expected, report.content)
 
-    @patch('katello.enabled_report.EnabledReport._obtain_mappings_dnf')
-    def test_var_interpolation_dnf(self, mock_obtain_mappings_dnf):
-        mock_obtain_mappings_dnf.return_value = {'releasever': "6.22", 'basearch': "80286"}
-        rh_repo = os.path.join(os.path.dirname(__file__), 'data/repos/redhat.repo.with_vars')
-        report = enabled_report.EnabledReport(rh_repo)
-        expected = {'enabled_repos': {'repos': [{'baseurl': ['https://enabled_repo.com/6.22/80286'], 'repositoryid': 'enabled_one'}]}}
-
-        self.assertEqual(expected, report.content)
-
-    @patch('katello.enabled_report.EnabledReport._obtain_mappings_yum')
-    @patch('katello.enabled_report.EnabledReport._obtain_mappings_dnf')
-    def test_var_interpolation_yum(self, mock_obtain_mappings_dnf, mock_obtain_mappings_yum):
-        mock_obtain_mappings_dnf.side_effect = ImportError("dnf not available on 80286")
-        mock_obtain_mappings_yum.return_value = {'releasever': "6.22", 'basearch': "80286"}
+    @unittest.skipIf('yum' not in sys.modules, "Yum not present")
+    @patch('katello.enabled_report.yum.YumBase.conf')
+    def test_var_interpolation_yum(self, yum_base_conf):
+        yum_base_conf.yumvar = {'releasever': "6.22", 'basearch': "80286"}
         rh_repo = os.path.join(os.path.dirname(__file__), 'data/repos/redhat.repo.with_vars')
         report = enabled_report.EnabledReport(rh_repo)
         expected = {'enabled_repos': {'repos': [{'baseurl': ['https://enabled_repo.com/6.22/80286'], 'repositoryid': 'enabled_one'}]}}
