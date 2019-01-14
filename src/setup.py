a3e1370c7b84d1a85369f7e1ec63c5b8d34bbd85
Jonathon Turel <jturel@gmail.com>
2019-01-14 10:07:43 -0500

    Release 3.4.2

--- src/setup.py
+++ src/setup.py
@@ -22,7 +22,7 @@ requires = [
 
 setup(
     name='katello-agent',
-    version='3.4.1',
+    version='3.4.2',
     description='Katello Agent',
     author='Jeff Ortel',
     author_email='jortel@redhat.com',
