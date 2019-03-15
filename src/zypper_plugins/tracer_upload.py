484ecdb0e486baf8f41f7ff06ee8970e88c487ba
Bernhard Suttner <suttner@atix.de>
2019-03-15 16:15:30 +0100

    Fixes #26375 - zypper plugin for tracer upload

---
+++ src/zypper_plugins/tracer_upload.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+#
+# Copyright 2019 ATIX AG
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+
+from os import path, environ
+import logging
+from katello.tracer import upload_tracer_profile
+from zypp_plugin import Plugin
+
+
+# The path is defined in zypper, see https://github.com/openSUSE/libzypp/blob/master/zypp/target/TargetImpl.cc
+REBOOT_NEEDED_FLAG = "/var/run/reboot-needed"
+
+class ZypperTracerApp:
+    pass
+
+class TracerUploadPlugin(Plugin):
+    def collect_apps(self, _plugin):
+        apps = []
+        if path.isfile(REBOOT_NEEDED_FLAG):
+            app = ZypperTracerApp()
+            app.name = "kernel"
+            app.helper = "You will have to reboot your computer"
+            app.type = "static"
+            apps.append(app)
+        return apps
+
+    def PLUGINEND(self, headers, body):
+        logging.info("PLUGINEND")
+
+        logging.info("Uploading Tracer Profile")
+        try:
+            upload_tracer_profile(self.collect_apps)
+        except:
+            logging.error("Unable to upload Tracer Profile")
+
+        self.ack()
+
+if __name__ == '__main__':
+    if "DISABLE_TRACER_UPLOAD_PLUGIN" in environ:
+        logging.info("$DISABLE_TRACER_UPLOAD_PLUGIN is set - disabling katello tracer upload plugin")
+
+        # a dummy Plugin is needed
+        plugin = Plugin()
+    else:
+        plugin = TracerUploadPlugin()
+
+    plugin.main()
