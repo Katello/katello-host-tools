Katello/katello-host-tools

commit 8c5b6c1ece2ef7ff76d781a2d5a41443c9ea0845

Rewrite scripts to use console_scripts entry points (#125)
* Drop EL5

The Python version in EL5 can no longer communicate with PyPI since it
doesn't support modern TLS. RHLE 5 also went EOL some time ago so it's
time to stop caring about this.

* Order images to newest first

This way the newest OS gets tested first, which is more likely to work
and be faster.

* Use dnf on Fedora

* Update EL6 to use Foreman 2.5 client tooling

While 2.5 is already end of life, this was the last release to support
it. That's better than 1.21.

* Update EL7 to use nightly client tools

Also pulls the image from quay and makes it a single transaction for
Foreman and EPEL.

* Update EL8 image to stream and nightly

* Only run flake8 on Python 3

flake8 4.0.0 requires Python >= 3.6 and this was the easiest way to keep
it working.

* Clean up metadata

This adds an URL which and removes the empty requires variable in favor
an inline definition.

* Rewrite scripts to use console_scripts entry points

This way Python makes the appropriate wrapper itself. This means it can
be uploaded to PyPI and consumed that way.

src/katello/scripts.py
@@ -1,0 +1,56 @@
+#!/usr/bin/python
+
+import optparse
+
+from katello.constants import (DISABLE_ENABLE_REPOS_VAR, DISABLE_PACKAGE_PROFILE_VAR,
+        ENABLED_REPOS_PLUGIN_CONF, PACKAGE_PROFILE_PLUGIN_CONF, REPOSITORY_PATH, YUM, ZYPPER,
+        ZYPPER_REPOSITORY_PATH)
+from katello.enabled_report import EnabledReport
+from katello.packages import purge_package_cache, upload_package_profile
+from katello.repos import EnabledRepoCache, upload_enabled_repos_report
+from katello.utils import combined_profiles_enabled
+
+
+def enabled_repos_upload():
+    description = 'This report can be disabled by either disabling the package management plugin in ' +\
+              ENABLED_REPOS_PLUGIN_CONF + ' or via the environment variable ' + DISABLE_ENABLE_REPOS_VAR
+    parser = optparse.OptionParser(description=description)
+    parser.add_option('-f', '--force', action='store_true',
+            help="Force enabled repository upload even if it does not seem out of date, or is otherwise disabled..")
+    (options, args) = parser.parse_args()
+    if options.force:
+        EnabledRepoCache.remove_cache()
+
+    report = None
+    if not combined_profiles_enabled():
+        if YUM:
+            repo_path = REPOSITORY_PATH
+        elif ZYPPER:
+            repo_path = ZYPPER_REPOSITORY_PATH
+        else:
+            raise IOError('Neither yum nor zypper can be used')
+        report = EnabledReport(repo_path)
+
+    upload_enabled_repos_report(report, options.force)
+
+
+def package_upload():
+    description = 'This report can be disabled by either disabling the package management plugin in ' + \
+              PACKAGE_PROFILE_PLUGIN_CONF + ' or via the environment variable ' + DISABLE_PACKAGE_PROFILE_VAR
+    parser = optparse.OptionParser(description=description)
+    parser.add_option('-f', '--force', action='store_true',
+            help="Force package upload even if it does not seem out of date.")
+
+    (options, args) = parser.parse_args()
+    if options.force:
+        purge_package_cache()
+    upload_package_profile(options.force)
+
+
+def tracer_upload():
+    try:
+        from katello import tracer
+    except ImportError:
+        raise SystemExit('Tracer is not supported on your platform')
+    else:
+        tracer.upload_tracer_profile(tracer.query_affected_apps, None)

.github/workflows/pull_request.yml
@@ -8,12 +8,11 @@ jobs:
       max-parallel: 1
       matrix:
         dockerfile:
-          - images/Dockerfile.el5
-          - images/Dockerfile.el6
-          - images/Dockerfile.el7
           - images/Dockerfile.el8
-          - images/Dockerfile.f33
+          - images/Dockerfile.el7
+          - images/Dockerfile.el6
           - images/Dockerfile.f34
+          - images/Dockerfile.f33
           - images/Dockerfile.suseLeap42
     steps:
       - uses: actions/checkout@v2

bin/katello-enabled-repos-upload
@@ -1,37 +1,0 @@
-#!/usr/bin/python
-
-import optparse
-
-from katello.constants import REPOSITORY_PATH, ZYPPER_REPOSITORY_PATH, ENABLED_REPOS_PLUGIN_CONF, DISABLE_ENABLE_REPOS_VAR, YUM, ZYPPER
-from katello.enabled_report import EnabledReport
-from katello.repos import EnabledRepoCache, upload_enabled_repos_report
-from katello.utils import combined_profiles_enabled
-
-def parse_args():
-    description = 'This report can be disabled by either disabling the package management plugin in ' +\
-              ENABLED_REPOS_PLUGIN_CONF + ' or via the environment variable ' + DISABLE_ENABLE_REPOS_VAR
-    parser = optparse.OptionParser(description=description)
-    parser.add_option('-f', '--force', help="Force enabled repository upload even if it does not seem out of date, or is otherwise disabled..", action='store_true')
-    return parser.parse_args()
-
-
-def main():
-    (options, args) = parse_args()
-    if options.force:
-        EnabledRepoCache.remove_cache()
-
-    report = None
-    if not combined_profiles_enabled():
-        if YUM:
-            repo_path = REPOSITORY_PATH
-        elif ZYPPER:
-            repo_path = ZYPPER_REPOSITORY_PATH
-        else:
-            raise IOError('Neither yum nor zypper can be used')
-        report = EnabledReport(repo_path)
-
-    upload_enabled_repos_report(report, options.force)
-
-
-if __name__ == "__main__":
-    main()

bin/katello-package-upload
@@ -1,37 +1,0 @@
-#!/usr/bin/python
-
-#
-# Copyright 2014 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-#
-
-import optparse
-
-from katello.packages import purge_package_cache, upload_package_profile
-from katello.constants import PACKAGE_PROFILE_PLUGIN_CONF, DISABLE_PACKAGE_PROFILE_VAR
-
-def parse_args():
-    description = 'This report can be disabled by either disabling the package management plugin in ' + \
-              PACKAGE_PROFILE_PLUGIN_CONF + ' or via the environment variable ' + DISABLE_PACKAGE_PROFILE_VAR
-    parser = optparse.OptionParser(description=description)
-    parser.add_option('-f', '--force', help="Force package upload even if it does not seem out of date.", action='store_true')
-    return parser.parse_args()
-
-
-def main():
-    (options, args) = parse_args()
-    if options.force:
-        purge_package_cache()
-    upload_package_profile(options.force)
-
-
-if __name__ == "__main__":
-    main()

bin/katello-tracer-upload
@@ -1,11 +1,0 @@
-#!/usr/bin/python
-
-from katello import tracer
-
-
-def main():
-    tracer.upload_tracer_profile(tracer.query_affected_apps, None)
-
-
-if __name__ == "__main__":
-    main()

containers/assets/epel-el5-archive.repo
@@ -1,9 +1,0 @@
-[epel-el5-archive]
-name=epel el5 archived
-baseurl=http://dl.fedoraproject.org/pub/archive/epel/5/x86_64/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=0
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1

images/Dockerfile.el5
@@ -1,34 +1,0 @@
-FROM astj/centos5-vault
-
-COPY containers/assets/epel-el5-archive.repo /etc/yum.repos.d/epel-el5-archive.repo
-RUN yum install -y wget make python-setuptools python-ctypes python-hashlib python-uuid python-simplejson dbus-python  pygobject2 python-dateutil python-dmidecode python-ethtool usermode virt-what python-dateutil && yum clean all
-
-# this repo provides a wget that support tls 1.1
-RUN wget http://www.tuxad.de/repo/5/tuxad.rpm
-RUN rpm -Uhv tuxad.rpm
-RUN yum upgrade -y wget --nogpgcheck && yum clean all
-
-RUN wget -r --no-parent https://fedorapeople.org/groups/katello/releases/yum/3.3/client/el5/x86_64/ --no-check-certificate
-WORKDIR /fedorapeople.org/groups/katello/releases/yum/3.3/client/el5/x86_64/
-RUN rpm -Uhv python-gofer-2.7.5-1.el5.noarch.rpm \
-             python-gofer-proton-2.7.5-1.el5.noarch.rpm \
-             python-qpid-proton-0.9-13.el5.x86_64.rpm \
-             python-qpid-0.18-10.el5.noarch.rpm \
-             saslwrapper-0.10-8.el5.x86_64.rpm \
-             python-saslwrapper-0.10-8.el5.x86_64.rpm \
-             qpid-proton-c-0.9-13.el5.x86_64.rpm \
-             subscription-manager-1.11.3-11.el5.x86_64.rpm \
-             python-rhsm-1.11.3-5.el5.x86_64.rpm \
-             python-pulp-agent-lib-2.8.0-0.7.beta.el5.noarch.rpm \
-             python-pulp-common-2.8.0-0.7.beta.el5.noarch.rpm \
-             python-isodate-0.5.0-4.pulp.el5.noarch.rpm \
-             gofer-2.7.5-1.el5.noarch.rpm --nosignature
-
-# if this disappears we can find it on github
-RUN wget http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el5/en/x86_64/rpmforge/RPMS/python-unittest2-0.5.1-1.el5.rf.noarch.rpm
-RUN rpm -Uhv python-unittest2-0.5.1-1.el5.rf.noarch.rpm
-
-# hard to get a mock rpm and pip ...
-RUN wget -O /usr/lib/python2.4/site-packages/mock.py https://raw.githubusercontent.com/testing-cabal/mock/1.0.1/mock.py --no-check-certificate
-
-WORKDIR /app

images/Dockerfile.el6
@@ -2,7 +2,8 @@ FROM centos:6
 
 RUN sed -i -e 's/^mirrorlist/#mirrorlist/g' -e 's/^#baseurl=http:\/\/mirror.centos.org\/centos\/$releasever\//baseurl=http:\/\/vault.centos.org\/6.10\//g' /etc/yum.repos.d/CentOS-Base.repo
 RUN yum install -y wget epel-release && yum clean all
-RUN yum install -y https://yum.theforeman.org/client/1.21/el6/x86_64/foreman-client-release.rpm && yum clean all
+# Foreman 2.5 was the last version to support EL6
+RUN yum install -y https://yum.theforeman.org/client/2.5/el6/x86_64/foreman-client-release.rpm && yum clean all
 RUN yum install make gofer python-gofer-proton python-pulp-agent-lib subscription-manager python-pip -y && yum clean all
 
 RUN yum install -y https://cbs.centos.org/kojifiles/packages/python-unittest2/0.5.1/3.el6/noarch/python-unittest2-0.5.1-3.el6.noarch.rpm && yum clean all

commit 9c2fde7a13099da03d9629b7c05d690526cf0bfa

Remove shebang from library
This isn't needed and triggers dependencies in RPM packaging that
shoulnd't be there.

Fixes: 8c5b6c1ece2ef7ff76d781a2d5a41443c9ea0845

src/katello/scripts.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import optparse
 
 from katello.constants import (DISABLE_ENABLE_REPOS_VAR, DISABLE_PACKAGE_PROFILE_VAR,